44c8ca4d0134ddf24ef0047ac0ff8c415b52c60d
Calixte Williams <calixte.williams@gmail.com>
2021-05-09 14:58:12 -0400

    Into to Python

---
+++ testenv/Kids.py
@@ -0,0 +1,25 @@
+children = "Tommy"
+_yutes = "Kim", "Sam", "Kez"
+
+
+print(children)
+print("These are my kids: ", _yutes )
+
+for kids in ["Caleb", "Sydney", "Savannah"]:
+    print("Clean your room,", kids, "!")
+
+# This is a comment.
+''' This is also a comment.'''
+""" This is also a comment, 
+    using 3 strings 
+    of Quatations."""
+
+print(2 * 2 * 2 * 2 * 2 * 2 * 2 * 2)
+
+# 0b or 0B to specify binary
+# 0o or 0O to specify Octal
+# 0x or 0X to specify hex
+
+print(0xbadbeef)
+print(hex(195935983))
+print(bin(195935983))
