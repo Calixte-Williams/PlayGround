c58e84820ed1149f73fa0dec00f5f03ae80f90b7
Calixte Williams <calixte.williams@gmail.com>
2021-05-13 22:07:03 -0400

    Added more Data Types

--- testenv/Kids.py
+++ testenv/Kids.py
@@ -1,5 +1,6 @@
 children = "Tommy"
-_yutes = "Kim", "Sam", "Kez"
+_yutes = ["Kim", "Tim", "Amanda"]
+
 
 
 print(children)
@@ -23,3 +24,37 @@ print(2 * 2 * 2 * 2 * 2 * 2 * 2 * 2)
 print(0xbadbeef)
 print(hex(195935983))
 print(bin(195935983))
+
+children = "Tommy"
+print(children[0:2])
+print("Jack" + children * 5)
+
+# To initialize a list in an empty state
+emptylist = []
+emptylist2 = list()
+
+print(_yutes[1])
+print(_yutes[2])
+print(_yutes[0])
+
+_yutes[2] = "Samantha"
+print(_yutes[2])
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+print(c[1:4])
+print(c[ :-4])
+print(c[:])
+
+#This is a "Tuple" data type. It is immutable, unlike a "List" or "String". Made using () and not [] like with "Lists"
+person = (2012, 'Mike', "CCNA")
+print(person)
+print(type(person))
+print(person[0])
+#Because a Tuple is immutable, you cannot make changes to the assigned variable indexes.
+""" person[0] = 2015 """
+#However it can be used to assign a set variables quickly
+(x, y, z) = (12, 'Fred', 18)
+print(z)
