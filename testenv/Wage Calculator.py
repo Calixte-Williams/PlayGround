d4c15726a9583b0cc925af04994a70150acc4a7e
Calixte Williams <calixte.williams@gmail.com>
2021-05-20 09:05:04 -0400

    Created a Euro Wage Calculator

---
+++ testenv/Wage Calculator.py
@@ -0,0 +1,11 @@
+wage = (((33000/12)/4)/40)
+eur = (3.30)
+xcd = wage * eur
+totalxcd = 33000 * eur
+parttime = (wage * 17) * 4
+ptxcd = parttime * eur
+
+print("Hourly pay in Germany is €", wage)
+print("Converted to XCD, this is equal to $", xcd)
+print("Average Gross Salary of an entry level IT help Desk employee is €33000. Converted to XCD, this is $", totalxcd)
+print("If I were to work a part-time job at this wage, I would make €", parttime ,". In XCD, this is $", ptxcd)
