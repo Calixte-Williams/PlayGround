34ee568baa35ca0b202a52fd7d668725c7976c90
Calixte Williams <calixte.williams@gmail.com>
2021-06-12 09:27:20 -0400

    Added - For Loops

--- testenv/Flow Control.py
+++ testenv/Flow Control.py
@@ -16,3 +16,13 @@ else:
     print('Number is invalid')
 # ELSE can be used at the end as a fallback if the IF / ELIF statements were not true.
 
+# For Loops
+dataset = (1, 2, 3, 4, 5)
+for variable in dataset:     # 'variable' here is a simply a placeholder variable to hold each sequence of data
+    print(variable)
+for x in range(3):    # range() used to iterate a specific number of times to run the loop, it starts from zero (0) & increments of one by default
+    print(x)
+for x in range(1,3):    # range(x, x) is used to indicate the number to start from
+    print(x)
+for x in range(1,11,3):     # A third attribute can be added to specify the increments to count by.
+    print(x)
