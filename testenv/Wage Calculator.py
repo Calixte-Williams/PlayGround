75cbdb76a8c835e68094b0a9eff585dbce2c0b8e
Calixte Williams <calixte.williams@gmail.com>
2021-09-25 08:00:08 -0400

    Minor Changes

--- testenv/Wage Calculator.py
+++ testenv/Wage Calculator.py
@@ -1,11 +1,19 @@
-wage = (((33000/12)/4)/40)
+#wage = (((33000/12)/4)/40)
 eur = (3.30)
+
+
+
+
+
+wage = int(input("Hourly pay in Germany is €"))
 xcd = wage * eur
-totalxcd = 33000 * eur
+print("Converted to XCD, this is equal to $", xcd)
+salary = (((wage * 40)*4)*12)
+totalxcd = salary * eur
+print("Average Gross Salary of an entry level IT help Desk employee is €", salary,". Converted to XCD, this is $", totalxcd)
 parttime = (wage * 17) * 4
 ptxcd = parttime * eur
-
-print("Hourly pay in Germany is €", wage)
-print("Converted to XCD, this is equal to $", xcd)
-print("Average Gross Salary of an entry level IT help Desk employee is €33000. Converted to XCD, this is $", totalxcd)
 print("If I were to work a part-time job at this wage, I would make €", parttime ,". In XCD, this is $", ptxcd)
+salary_parttime = parttime * 12
+sptxcd = salary_parttime * eur
+print("Yearly salary would be €15", salary_parttime," and $", sptxcd," in xcd.")
